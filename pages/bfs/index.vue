b56751e7ec89725eb1ea16532c3af040d6939e1c
lzy1960 <1960553899@qq.com>
2022-09-06 11:35:09 +0800

    feat: bfs complete

--- pages/bfs/index.vue
+++ pages/bfs/index.vue
@@ -1,49 +1,37 @@
 <template>
   <div class="bfs">
     <button @click="path(map, start, end)">start</button>
-    <div v-for="y in 20" :key="y">
-      <div :ref="el => curEl = el" v-for="x in 30" :key="x" inline-block w-5 h-5 bg-gray-7 bg-op-30 m-2px cursor-pointer
-        hover:bg-op-80>
-        <span>{{ isHighlight(x, y) ? 'x' : '' }}</span>
+    <div v-for="_, y in HEIGHT" :key="y">
+      <div v-for="_, x in WIDTH" :key="`${x}_${y}`" inline-block w-5 h-5 bg-gray-7 bg-op-30 m-2px cursor-pointer
+        hover:bg-op-80 :class="map[y]?.[x] === 1 ? 'bg-red' : ''">
+        <span>{{ map[y]?.[x] }}</span>
       </div>
     </div>
   </div>
 </template>
 
 <script lang="ts" setup>
-
-const WIDTH = 30
-const HEIGHT = 20
-const map = Array(HEIGHT).fill(0).map(v => Array(WIDTH).fill(0))
+const WIDTH = ref(30)
+const HEIGHT = ref(20)
+const map = ref(Array(HEIGHT.value).fill(0).map(v => Array(WIDTH.value).fill(0)))
 const start = [2, 2]
 const end = [10, 10]
-const isHighlight = (x, y) => {
-  const isStart = x === start[0] && y === start[1]
-  const isEnd = x === end[0] && y === end[1]
-  return isStart || isEnd
-}
-const curEl = ref()
 function sleep (t: number) {
   return new Promise(function (resolve) {
     setTimeout(resolve, t);
   });
 }
-const path = async (map: number[][], start: number[], end: number[]) => {
-  console.log(start)
+const path = async (map, start: number[], end: number[]) => {
   const queue = [start]
   async function insert (x: number, y: number) {
     // 越界
-    if (x < 0 || x >= WIDTH || y < 0 || y >= HEIGHT) return
+    if (x < 0 || x >= WIDTH.value || y < 0 || y >= HEIGHT.value || map[y]?.[x] === 1) return
     await sleep(1)
     map[y][x] = 1
     queue.push([x, y])
   }
   while (queue.length) {
     let [x, y] = queue.shift()
-    console.log(queue)
-    console.log(x, y)
-    console.log(curEl)
-    curEl.value.style.backgroundColor = '#f00'
     // 到终点
     if (x === end[0] && y === end[1]) return true
     await insert(x, y - 1)
@@ -53,7 +41,6 @@ const path = async (map: number[][], start: number[], end: number[]) => {
   }
   return false
 }
-// console.log(path(map, start.value, end.value))
 </script>
 
 <style lang='scss' scoped>
