1db3a09413bc92b0633e8b713b078026b94d2d76
lzy1960 <1960553899@qq.com>
2022-09-06 14:49:36 +0800

    feat: find shortest path

--- pages/bfs/index.vue
+++ pages/bfs/index.vue
@@ -1,10 +1,12 @@
 <template>
   <div class="bfs">
-    <button @click="path(map, start, end)">start</button>
-    <div v-for="_, y in HEIGHT" :key="y">
-      <div v-for="_, x in WIDTH" :key="`${x}_${y}`" inline-block w-5 h-5 bg-gray-7 bg-op-30 m-2px cursor-pointer
-        hover:bg-op-80 :class="map[y]?.[x] === 1 ? 'bg-red' : ''">
-        <span>{{ map[y]?.[x] }}</span>
+    <button @click="findPath(map, start, end)">start</button>
+    <div :ref="el => containerEl = el" class="container">
+      <div v-for="_, y in HEIGHT" :key="y">
+        <div v-for="_, x in WIDTH" :key="`${x}_${y}`" inline-block w-5 h-5 bg-gray-7 bg-op-30 m-2px cursor-pointer
+          hover:bg-op-80 :class="map[y]?.[x] ? 'bg-red' : ''">
+          <span>{{ map[y]?.[x] ? 'x' : '' }}</span>
+        </div>
       </div>
     </div>
   </div>
@@ -16,30 +18,48 @@ const HEIGHT = ref(20)
 const map = ref(Array(HEIGHT.value).fill(0).map(v => Array(WIDTH.value).fill(0)))
 const start = [2, 2]
 const end = [10, 10]
+const containerEl = ref()
 function sleep (t: number) {
   return new Promise(function (resolve) {
     setTimeout(resolve, t);
   });
 }
-const path = async (map, start: number[], end: number[]) => {
+const findPath = async (map, start: number[], end: number[]) => {
+  const table = Object.create(map)
   const queue = [start]
-  async function insert (x: number, y: number) {
+  async function insert (x: number, y: number, pre) {
+    // console.log(pre)
     // 越界
-    if (x < 0 || x >= WIDTH.value || y < 0 || y >= HEIGHT.value || map[y]?.[x] === 1) return
+    if (x < 0 || x >= WIDTH.value || y < 0 || y >= HEIGHT.value || table[y][x]) return
     await sleep(1)
-    map[y][x] = 1
+    table[y][x] = pre
     queue.push([x, y])
   }
   while (queue.length) {
     let [x, y] = queue.shift()
     // 到终点
-    if (x === end[0] && y === end[1]) return true
-    await insert(x, y - 1)
-    await insert(x, y + 1)
-    await insert(x - 1, y)
-    await insert(x + 1, y)
+    if (x === end[0] && y === end[1]) {
+      let path = []
+      while(x !== start[0] || y !== start[1]) {
+        console.log(map[y][x])
+        console.log(table)
+        console.log(path)
+        // [x, y] = table[y][x]
+        x = table[y][x][0]
+        y = table[y][x][1]
+        path.push(map[y][x])
+        await sleep(1)
+        // map[b][a] = 1
+        containerEl.value.children[y].children[x].style.backgroundColor = 'skyblue'
+      }
+      return path
+    }
+    await insert(x, y - 1, [x, y])
+    await insert(x, y + 1, [x, y])
+    await insert(x - 1, y, [x, y])
+    await insert(x + 1, y, [x, y])
   }
-  return false
+  return null
 }
 </script>
 
