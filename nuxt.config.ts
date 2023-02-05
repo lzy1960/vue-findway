782fc2bc8cbc8b3a4f88d5998bbc500adfa0933a
lzy1960 <1960553899@qq.com>
2023-02-05 20:08:17 +0800

    feat: meta

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,9 +1,23 @@
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
+  ssr: true,
   app: {
     head: {
       viewport: 'width=device-width',
       title: 'Vue3 Findway',
+      meta: [
+        { charset: 'utf-8' },
+        {
+          key: 'description',
+          name: 'description',
+          content: 'Vue3 findway visual',
+        },
+        {
+          key: 'name',
+          name: 'google-site-verification',
+          content: 'W1WLqWMDmqeXOHA0O0K7tSRDbNlJQoGp33OQ2txuhmU',
+        },
+      ],
       link: [
         { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
       ],
