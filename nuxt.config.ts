c8d6e65c7c92b81673cd1c9f278fb15cd58f8455
lzy1960 <1960553899@qq.com>
2023-02-05 17:40:34 +0800

    feat: add favicon

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,14 @@
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
+  app: {
+    head: {
+      viewport: 'width=device-width',
+      title: 'Vue3 Findway',
+      link: [
+        { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+      ],
+    },
+  },
   modules: [
     '@unocss/nuxt',
     '@vueuse/nuxt',
